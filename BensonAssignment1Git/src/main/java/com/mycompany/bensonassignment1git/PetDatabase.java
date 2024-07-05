f40a401355de37efa27b730f1da7291bba79fbdd
PhillipBen <157083488+PhillipBen@users.noreply.github.com>
2024-07-05 13:57:01 -0500

    Commit 1
    Fix

--- BensonAssignment1Git/src/main/java/com/mycompany/bensonassignment1git/PetDatabase.java
+++ BensonAssignment1Git/src/main/java/com/mycompany/bensonassignment1git/PetDatabase.java
@@ -17,15 +17,6 @@ public class PetDatabase {
         petList.add(pet);
     }
     
-    public void Update(int index, Pet newPet) {
-        petList.remove(index);
-        petList.add(index, newPet);
-    }
-    
-    public void Remove(int index) {
-        petList.remove(index);
-    }
-    
     public void PrintDatabase() {
         //Print the petList from this class
         System.out.println("");
@@ -38,37 +29,5 @@ public class PetDatabase {
         System.out.println("+----------------------+");
         System.out.println("" + petList.size() + " rows in set.");
         System.out.println("");
-    }
-    
-    public void PrintDatabase(ArrayList<Pet> arrayList) {
-        //Print an input arrayList, given it is an ArrayList that contains Pet(s).
-        System.out.println("");
-        System.out.println("+----------------------+");
-        System.out.println("| ID | NAME      | AGE |");
-        System.out.println("+----------------------+");
-        for (int i = 0; i < arrayList.size(); i++) {
-            System.out.printf("%-5s%-12s%-6s%-1s%n", "| " + i, "| " + arrayList.get(i).GetName(), "| " + arrayList.get(i).GetAge(), "|"); 
-        }
-        System.out.println("+----------------------+");
-        System.out.println("" + arrayList.size() + " rows in set.");
-        System.out.println("");
-    }
-    
-    public void SearchDatabase(int searchCase, String searchString) {
-        //Searches the petList for a specific name or age and prints all found results.
-        //searchCase: 0 = name, 1 = age
-        ArrayList<Pet> tempPetList = new ArrayList<>();
-        if(searchCase == 0) {
-            //name
-            for (int i = 0; i < petList.size(); i++) 
-                if (petList.get(i).GetName().toLowerCase().equals(searchString.toLowerCase()))
-                    tempPetList.add(petList.get(i));
-        }else {
-            //age
-            for (int i = 0; i < petList.size(); i++) 
-                if (petList.get(i).GetAge() == Integer.parseInt(searchString))
-                    tempPetList.add(petList.get(i));
-        }
-        PrintDatabase(tempPetList);
     }
 }
